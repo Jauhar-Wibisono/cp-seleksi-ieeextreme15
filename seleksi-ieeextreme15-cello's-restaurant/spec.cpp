91e31237b80911c88b5723619d5c63b0a86d8613
Marcello Faria <01faria.marcello@gmail.com>
2021-09-27 23:09:59 +0700

    tinggal tc

--- seleksi-ieeextreme15-cello's-restaurant/spec.cpp
+++ seleksi-ieeextreme15-cello's-restaurant/spec.cpp
@@ -4,27 +4,48 @@ using namespace tcframe;
 class ProblemSpec : public BaseProblemSpec
 {
 protected:
-  int N, ans;
+  int N;
+  string op;
+  vector<int> op_num, ans;
 
   void InputFormat()
   {
     LINE(N);
+    LINES(op, op_num) % SIZE(N);
   }
 
   void OutputFormat()
   {
-    LINE(ans);
+    LINES(ans) % SIZE(N);
   }
 
   void GradingConfig()
   {
     TimeLimit(1);
-    MemoryLimit(64);
+    MemoryLimit(256);
   }
 
   void Constraints()
   {
-    CONS(1 <= N <= 1e18);
+    CONS(1 <= N <= 1e9);
+    CONS(isValidOperationName(op));
+    CONS(isValidOperationNumber(N, op_num));
+  }
+
+private:
+  bool isValidOperationName(string s)
+  {
+    return (s == "antri" || s == "selesai" || s == "traktir");
+  }
+
+  bool isValidOperationNumber(int n, vector<int> a)
+  {
+    bool ret = a.size() == n;
+    for (int i = 0; i < a.size(); i++)
+    {
+      ret = ret && (1 <= a[i] && a[i] <= 1e9);
+    }
+    return ret;
   }
 };
 
@@ -33,20 +54,28 @@ class TestSpec : public BaseTestSpec<ProblemSpec>
 protected:
   void SampleTestCase1()
   {
-    Input({"1"});
-    Output({"1"});
-  }
-
-  void SampleTestCase1()
-  {
-    Input({"3"});
-    Output({"3"});
+    Input({"12",
+           "antri 4",
+           "selesai"
+           "antri 3",
+           "antri 5",
+           "antri 2",
+           "traktir 3 1",
+           "selesai",
+           "antri 1",
+           "traktir 2 2",
+           "antri 4"
+           "selesai"
+           "selesai"});
+    Output({"4 EMPTY 3 5 2 3 6 1 1 4 1 8"});
   }
 
   void TestCases()
   {
     for (int i = 0; i < 10; i++)
     {
+      vector<string> op;
+
       CASE(N = rnd.nextInt(1, 1000));
     }
     for (int i = 0; i < 20; i++)
